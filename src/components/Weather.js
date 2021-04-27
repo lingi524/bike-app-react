b5dc4cd13eef0494c148679e30a156c840fae0dd
lingi524 <lingi524@student.liu.se>
2021-04-27 16:03:29 +0200

    /

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -40,7 +40,7 @@ function Weather () {
    
     return (
         <div className="weatherText">
-            <p>Should you go for a ride in {cityName}?</p>
+            <p>Should you go for a ride in{cityName}?</p>
             <p>Right now there's {weatherType} and {temperature}°</p>
             <p>It feels like {perceivedTemperature}°</p>
             <Answer cityName={cityName} />
